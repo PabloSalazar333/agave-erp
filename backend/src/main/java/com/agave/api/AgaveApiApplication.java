03139f924c7fee0048dc5b53ebdfcc92ac2d73db
Pablo Esteban Salazar Márquez <p.e.salazarmarquez@gmail.com>
2025-08-25 21:46:47 -0600

    feat(api): hello endpoint + actuator (health/info) + app info

---
+++ backend/src/main/java/com/agave/api/AgaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.agave.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgaveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgaveApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/agave/api/AgaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agave.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgaveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
